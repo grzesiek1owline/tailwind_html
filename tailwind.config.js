563b896a86dde8855cc84568f15e51e8e27babf4
Grzegorz Rydzkowski <grzesiek.rydzkowski@gmail.com>
2022-01-19 09:36:17 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: [
+        "./src/**/*.{html,jsx}",
+        "./*.{html,jsx}"
+    ], //paths to files where tailwind find framework css classes
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
+// Other example
+// module.exports = {
+//     content: [
+//         './app/**/*.php',
+//         './resources/**/*.php',
+//         './resources/**/*.vue',
+//         './resources/**/*.js',
+//     ],
